029a1e45b5fae472c0fd9cda2f92771a366b29c1
Your Name <you@example.com>
2024-10-07 00:24:31 +0100

    completed pascal triangle problem

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    if n == 0:
+        return []
+
+    pascal = [[1]]
+
+    if n > 1:
+        pascal.append([1, 1])
+
+    for i in range(2, n):
+        prev_row = pascal[i - 1]
+        current_row = [1]
+
+
+        for j in range(1, i):
+            current_row.append(prev_row[j - 1] + prev_row[j])
+
+        current_row.append(1)
+        pascal.append(current_row)
+
+    return pascal
